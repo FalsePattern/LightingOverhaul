2efbc882c5f4ca1d40f80b9dc2f3834259b797a7
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-06 14:31:43 +0100

    Fix incompatibility with Windows operating systems

--- src/main/java/lightingoverhaul/helper/shader/common/ShaderHelper.java
+++ src/main/java/lightingoverhaul/helper/shader/common/ShaderHelper.java
@@ -1,5 +1,6 @@
 package lightingoverhaul.helper.shader.common;
 
+import lightingoverhaul.CoreLoadingPlugin;
 import lombok.val;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
@@ -23,35 +24,26 @@ public class ShaderHelper {
         }
     }
 
-    public static int createShader(String vertexSource, String fragmentSource) {
-        int vertexShader = compileShader(vertexSource, GL20.GL_VERTEX_SHADER);
-        try {
-            int fragmentShader = compileShader(fragmentSource, GL20.GL_FRAGMENT_SHADER);
-            try {
-                return createProgram(vertexShader, fragmentShader);
-            } finally {
-                GL20.glDeleteShader(fragmentShader);
-            }
-        } finally {
-            GL20.glDeleteShader(vertexShader);
-        }
-    }
-
     private static int compileShader(String source, int shaderType) {
-        int shader = GL20.glCreateShader(shaderType);
+        val shader = GL20.glCreateShader(shaderType);
         GL20.glShaderSource(shader, source);
         GL20.glCompileShader(shader);
-        String infoString = GL20.glGetShaderInfoLog(shader, 2000);
-        if (GL11.glGetError() != GL11.GL_NO_ERROR || GL20.glGetShaderi(shader, GL20.GL_COMPILE_STATUS) != GL11.GL_TRUE || !infoString.equals("")) {
+        val infoLog = GL20.glGetShaderInfoLog(shader, GL20.glGetShaderi(shader, GL20.GL_INFO_LOG_LENGTH));
+        if (GL11.glGetError() != GL11.GL_NO_ERROR || GL20.glGetShaderi(shader, GL20.GL_COMPILE_STATUS) != GL11.GL_TRUE) {
             GL20.glDeleteShader(shader);
-            throw new ShaderException("Error while compiling shader: \n" + infoString);
+            throw new ShaderException("Error while compiling shader: \n" + (infoLog.equals("") ? "No Details Available" : infoLog));
+        } else if (!"".equals(infoLog)) {
+            CoreLoadingPlugin.CLLog.warn("Warnings detected while compiling shader:");
+            for (val line: infoLog.split("\\n")) {
+                CoreLoadingPlugin.CLLog.warn(line);
+            }
         }
         return shader;
     }
 
     private static int createProgram(int... shaders) {
-        int program = GL20.glCreateProgram();
-        for (int shader: shaders) {
+        val program = GL20.glCreateProgram();
+        for (val shader: shaders) {
             GL20.glAttachShader(program, shader);
         }
         GL20.glLinkProgram(program);
@@ -60,18 +52,23 @@ public class ShaderHelper {
         GL20.glValidateProgram(program);
         verifyProgram(program, GL20.GL_VALIDATE_STATUS, "Error while validating shader program");
 
-        for (int shader: shaders) {
+        for (val shader: shaders) {
             GL20.glDetachShader(program, shader);
         }
         return program;
     }
 
     private static void verifyProgram(int program, int programI, String message) {
-        String infoLog = GL20.glGetProgramInfoLog(program, 2000);
-        int status = GL20.glGetProgrami(program, programI);
-        if (GL11.glGetError() != GL11.GL_NO_ERROR || status != GL11.GL_TRUE || !infoLog.equals("")) {
+        val status = GL20.glGetProgrami(program, programI);
+        val infoLog = GL20.glGetProgramInfoLog(program, GL20.glGetProgrami(program, GL20.GL_INFO_LOG_LENGTH));
+        if (GL11.glGetError() != GL11.GL_NO_ERROR || status != GL11.GL_TRUE) {
             GL20.glDeleteProgram(program);
             throw new ShaderException(message + ": \n" + (infoLog.equals("") ? "No Details Available" : infoLog));
+        } else if (!"".equals(infoLog)) {
+            CoreLoadingPlugin.CLLog.warn("Warnings detected while linking or verifying shader:");
+            for (val line: infoLog.split("\\n")) {
+                CoreLoadingPlugin.CLLog.warn(line);
+            }
         }
     }
 
