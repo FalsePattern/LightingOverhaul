fd384fc44fd2002e3c223877bb8393528805f349
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-09 02:14:51 +0100

    Better description for the magic class

--- src/main/java/coloredlightscore/src/asm/ColoredLightsCoreLoadingPlugin.java
+++ src/main/java/coloredlightscore/src/asm/ColoredLightsCoreLoadingPlugin.java
@@ -1,7 +1,12 @@
 package coloredlightscore.src.asm;
 
 /**
- * Fastcraft compatibility
+ * FastCraft compatibility.
+ * Use this class for detecting the presence of LightingOverhaul. This class is guaranteed to exist in every future
+ * version. This class is an artifact from when LightingOverhaul was still the ColoredLights mod. Due to the class
+ * structure change, FastCraft could not detect the mod. FastCraft looks for this class on the classpath for enabling
+ * internal compatibility. Due to FastCraft being unnecessarily obfuscated, this compatibility tweak cannot be done in
+ * the other direction, so this class now stays here permanently.
  */
 public class ColoredLightsCoreLoadingPlugin {
     public static void touch() {
